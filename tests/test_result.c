8fe32d993f8e29cc8b048b0191824e7a75095cb8
CohenArthur <arthur.cohen@epita.fr>
2020-08-07 02:48:28 +0200

    result: Add test for aggregate()

--- tests/test_result.c
+++ tests/test_result.c
@@ -1,5 +1,7 @@
 #include <assert.h>
 #include <string.h>
+#include <stdlib.h>
+
 #include "../src/result.h"
 
 #define TEST(Name, Subname) void test_##Name##_##Subname()
@@ -24,9 +26,36 @@ TEST(result, bad)
     ASSERT_TRUE(res_is_bad(&res));
 }
 
+static char *str_concat(char *old, char *new)
+{
+    size_t old_len = strlen(old);
+    size_t new_len = strlen(new);
+
+    old = realloc(old, old_len + new_len + 1);
+
+    memcpy(old + old_len, new, new_len);
+    old[new_len + old_len] = 0;
+
+    return old;
+}
+
+TEST(result, aggregate)
+{
+    char *err_msg = calloc(1, 256);
+    memcpy(err_msg, "Result<", 7);
+
+    struct result res = res_bad_new(err_msg);
+
+    res_bad_aggregate(&res, "Option<T>>", str_concat);
+
+    ASSERT_EQ(strcmp(res_bad(&res), "Result<Option<T>>"), 0);
+
+    free(res_bad(&res));
+}
+
 int main(void)
 {
     RUN_TEST(result, good);
     RUN_TEST(result, bad);
-    // RUN_TEST(result, bad_get);
+    RUN_TEST(result, aggregate);
 }
